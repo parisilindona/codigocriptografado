0a433353fcdc7105d01954f6cc6ff0d8f801f69c
parisi <160138803+parisilindona@users.noreply.github.com>
2024-03-01 14:38:27 -0300

    Add files via upload

---
+++ dia1/inicio.js
@@ -0,0 +1,11 @@
+// alert -> cria uma janela de alerta no navegador  
+alert  ('oi')
+// prompt -> cria uma caixa de texto
+prompt ('digite seu sobrenome')
+// let e const -> são usados para declarar variáveis
+// const não mexe e let altera
+
+let nome;
+
+nome = prompt ('digite seu nome');
+alert(`seu nome é: ${nome}`)
